533c102802a2b3d2820ae0bd6c8287aa7f267cb0
Ryan George <mailryangeorge@gmail.com>
2011-07-28 18:05:03 -0500

    Wrapping up Thursday

--- Prototype2/CameraViewController.h
+++ Prototype2/CameraViewController.h
@@ -9,8 +9,62 @@
 #import <UIKit/UIKit.h>
 
 
-@interface CameraViewController : UIViewController {
-
+@interface CameraViewController : UIViewController 
+{
+	IBOutlet UIImageView *mainImage;
+	IBOutlet UIButton *choosePhoto;
+	IBOutlet UIButton *takePhoto;
+	
+	IBOutlet UIView *studentView;
+	IBOutlet UIView *projectView;
+	IBOutlet UIView *contentView;
+	IBOutlet UIView *summaryView;
+	
+	IBOutlet UISegmentedControl *userSelect;
+	IBOutlet UISegmentedControl *documentCollect;
+	
+	IBOutlet UIButton *studentButton;
+	IBOutlet UIButton *projectButton;
+	IBOutlet UIButton *contentButton;
+	IBOutlet UIButton *summaryButton;
+	IBOutlet UIButton *findPhoto;
+	IBOutlet UIButton *saveButton;
+	
+	IBOutlet UILabel *tagsLabel;
+	
+	IBOutlet UIScrollView *studentScroll;
+	IBOutlet UIScrollView *projectScroll;
+	IBOutlet UIScrollView *contentScroll;
+	IBOutlet UIScrollView *summaryScroll;
 }
+
+@property (nonatomic, retain) IBOutlet UIImageView *mainImage;
+@property (nonatomic, retain) IBOutlet UIButton *choosePhoto;
+@property (nonatomic, retain) IBOutlet UIButton *takePhoto;
+@property (nonatomic, retain) IBOutlet UIView *studentView;
+@property (nonatomic, retain) IBOutlet UIView *projectView;
+@property (nonatomic, retain) IBOutlet UIView *contentView;
+@property (nonatomic, retain) IBOutlet UIView *summaryView;
+@property (nonatomic, retain) IBOutlet UISegmentedControl *userSelect;
+@property (nonatomic, retain) IBOutlet UIButton *studentButton;
+@property (nonatomic, retain) IBOutlet UIButton *projectButton;
+@property (nonatomic, retain) IBOutlet UIButton *contentButton;
+@property (nonatomic, retain) IBOutlet UIButton *summaryButton;
+@property (nonatomic, retain) IBOutlet UILabel *tagsLabel;
+@property (nonatomic, retain) IBOutlet UISegmentedControl *documentCollect;
+@property (nonatomic, retain) IBOutlet UIButton *findPhoto;
+@property (nonatomic, retain) IBOutlet UIButton *saveButton;
+@property (nonatomic, retain) IBOutlet UIScrollView *studentScroll;
+@property (nonatomic, retain) IBOutlet UIScrollView *projectScroll;
+@property (nonatomic, retain) IBOutlet UIScrollView *contentScroll;
+@property (nonatomic, retain) IBOutlet UIScrollView *summaryScroll;
+
+-(IBAction) capturePhoto:(id) sender;
+
+-(IBAction) showStudents:(id) sender;
+-(IBAction) showProjects:(id) sender;
+-(IBAction) showContent:(id) sender;
+-(IBAction) showSummary:(id) sender;
+-(IBAction) toggleUser:(id) sender;
 
 @end
--- Prototype2/CameraViewController.m
+++ Prototype2/CameraViewController.m
@@ -11,6 +11,124 @@
 
 @implementation CameraViewController
 
+@synthesize mainImage;
+@synthesize choosePhoto;
+@synthesize takePhoto;
+@synthesize studentView, projectView, contentView, summaryView;
+@synthesize studentButton, 
+			projectButton, 
+			contentButton, 
+			summaryButton, 
+			findPhoto, 
+			saveButton, 
+			tagsLabel, 
+			documentCollect,
+			studentScroll,
+			projectScroll,	
+			contentScroll,
+			summaryScroll;
+
+-(void)viewDidLoad
+{
+	[studentScroll setScrollEnabled:YES];
+	[studentScroll setContentSize:CGSizeMake(1334, 149)];
+	
+	[projectScroll setScrollEnabled:YES];
+	[projectScroll setContentSize:CGSizeMake(880, 149)];
+	
+	[contentScroll setScrollEnabled:YES];
+	[contentScroll setContentSize:CGSizeMake(1230, 149)];
+	
+	[summaryScroll setScrollEnabled:YES];
+	[summaryScroll setContentSize:CGSizeMake(700, 149)];
+}
+
+-(IBAction) capturePhoto:(id) sender
+{
+	UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+	picker.delegate = self;
+	
+	if((UIButton *) sender == choosePhoto)
+	{
+		picker.sourceType = UIImagePickerControllerSourceTypeSavedPhotosAlbum;
+	}
+	else
+	{
+		picker.sourceType = UIImagePickerControllerSourceTypeCamera;
+	}
+	
+	[self presentModalViewController:picker animated:YES];
+
+}
+
+-(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+	[picker dismissModalViewControllerAnimated:YES];
+	mainImage.image = [info objectForKey:@"UIImagePickerControllerOriginalImage"];
+}
+
+-(IBAction)toggleUser:(id) sender
+{
+	if(userSelect.selectedSegmentIndex == 1)
+	{
+		[studentView setHidden:YES];
+		[projectView setHidden:YES];
+		[contentView setHidden:YES];
+		[summaryView setHidden:YES];
+		
+		[studentButton setHidden:YES];
+		[projectButton setHidden:YES];
+		[contentButton setHidden:YES];
+		[summaryButton setHidden:YES];
+		
+		[tagsLabel setHidden:YES];
+		[findPhoto setHidden:YES];
+		[saveButton setHidden:YES];
+		[documentCollect setHidden:YES];
+	}
+	else
+	{
+		[tagsLabel setHidden:NO];
+		[studentButton setHidden:NO];
+		[projectButton setHidden:NO];
+		[contentButton setHidden:NO];
+		[summaryButton setHidden:NO];
+		[saveButton setHidden:NO];
+		[findPhoto setHidden:NO];
+		[documentCollect setHidden:NO];
+	}
+
+}
+
+-(IBAction) showStudents:(id) sender
+{
+	[studentView setHidden:NO];
+	[projectView setHidden:YES];
+	[contentView setHidden:YES];
+	[summaryView setHidden:YES];
+}
+-(IBAction) showProjects:(id) sender
+{
+	[studentView setHidden:YES];
+	[projectView setHidden:NO];
+	[contentView setHidden:YES];
+	[summaryView setHidden:YES];
+}
+-(IBAction) showContent:(id) sender
+{
+	[studentView setHidden:YES];
+	[projectView setHidden:YES];
+	[contentView setHidden:NO];
+	[summaryView setHidden:YES];
+}
+-(IBAction) showSummary:(id) sender
+{
+	[studentView setHidden:YES];
+	[projectView setHidden:YES];
+	[contentView setHidden:YES];
+	[summaryView setHidden:NO];
+}
+
  // The designated initializer.  Override if you create the controller programmatically and want to perform customization that is not appropriate for viewDidLoad.
 /*
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
